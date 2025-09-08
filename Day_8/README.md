16a7edcb082e130175ab0983cad6712fd339805c
Ayomikun Olalekan <ayomikunolalekan6@gmail.com>
2025-09-08 17:30:57 +0100

    feat: Added day 8 project

---
+++ Day_8/README.md
@@ -0,0 +1,6 @@
+## Project Overview  
+This project is an affirmation generator that makes use of the concepts of if, elif, else and nested if statements. It also makes use of the input and print statement.  
+The user inputs their name and what day of the week it is and a positive affirmation is printed out to the user.  
+
+## Areas of Improvement  
+Areas of improvement could be to add specific affirmations for certain individuals while leaving the general affirmations for others. The concatenation could also be done better but that is a feature that'll change in my projects after learning f strings in python.
---
+++ Day_8/main.py
@@ -0,0 +1,21 @@
+print("🌟✨✨ Affirmation Generator 🌟✨✨")
+print()
+name = input("Hi there!! What is your name?: ")
+dayOfTheWeek = input("What day of the week is it?: ")
+
+if dayOfTheWeek == "Sunday" or dayOfTheWeek == "sunday":
+    print("Hey", name, ",it's the first day of the week, rest up and have fun because you're going to have an amazing week ❤️")
+elif dayOfTheWeek == "Monday" or dayOfTheWeek == "monday":
+  print("Hi", name, ", first day of the work week, really scary right but just know this, you can do it and failure isn't fatal but failure to change might be ❤️")
+elif dayOfTheWeek == "Tuesday" or dayOfTheWeek == "tuesday":
+    print("Hey", name, ", you are a star and you will shine brightly ❤️")
+elif dayOfTheWeek == "Wednesday" or dayOfTheWeek == "wednesday":
+    print("Hola", name, ", middle of the work, keep pushing, I'm rooting for you ❤️")
+elif dayOfTheWeek == "Thursday" or dayOfTheWeek == "thursday":
+    print("Konichiwa", name, ", how have you been so far, hopefully good. You've got this and know that I care about you ❤️")
+elif dayOfTheWeek == "Friday" or dayOfTheWeek == "friday":
+    print("Hey", name, ", TGIF, get through today and enjoy your weekend, you really deserve the best ❤️")
+elif dayOfTheWeek == "Saturday" or dayOfTheWeek == "saturday":
+    print("Hello", name, ", weekend vibes, just chill out, spend time with family and have fun ❤️")
+else:
+    print("I'm sorry that day doesn't exist but I hope everything goes well for you today.")
