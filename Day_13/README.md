f76766a49c0f72869e9f0c10251a2c3b14f19c90
Ayomikun Olalekan <ayomikunolalekan6@gmail.com>
2025-09-15 04:38:45 +0100

    feat: Added Day 13 project

---
+++ Day_13/README.md
@@ -0,0 +1,6 @@
+## Project Overview  
+This project is a gradebook or a grade generator. It makes use of int and float functions to convert input function values to numbers. It also makes use of if, elif and else statements to determine the user's grade based on a certain percentage range and mathematical operators to perform calculations.  
+The user inputs the total score they could have gotten in a test and their score, based on those values, the program calculates their score percentage and uses the range of values to determine their grade.  
+
+# Areas of Improvement  
+1. The program could ask the user for the grading criteria instead of just using the hardcoded grading values or ranges to determine the user's grade. This would help out as grading varies by schools, regions and countries.  
---
+++ Day_13/main.py
@@ -0,0 +1,22 @@
+print("Gradebook 📕📕")
+print()
+course = input("What course or subject were your exams on?: ")
+maxScore = int(input("What is the maximum number of points you could have gotten on the test?: "))
+userScore = float(input("What is your score on the test?: "))
+
+scorePercentage = (userScore / maxScore) * 100
+scorePercentage = round(scorePercentage, 2)
+print("Based on your score, your total percentage is", scorePercentage)
+
+if scorePercentage >= 90 and scorePercentage <= 100:
+  print("Congratulations 🥳🥳🥳, you got an A+ on your", course, "test. I'm really proud of you.")
+elif scorePercentage >= 80 and scorePercentage <= 89:
+  print("You got an A on your", course, " test, congratulations 🥳🥳🥳")
+elif scorePercentage >= 70 and scorePercentage <= 79:
+  print("You got a B on your", course, " test, congratulations my friend, you did good 🥳🥳🥳")
+elif scorePercentage >= 60 and scorePercentage <= 69:
+  print("You got a C on your", course, " test, you did good, I'm happy for you 🥳")
+elif scorePercentage >= 50 and scorePercentage <= 59:
+  print("You got a D on your", course, " test 🥲, you tried but you could have done better my friend")
+else:
+  print("You got a U on your", course, " test, I believe that is an ungraded. I'm really my friend, better luck next time.")
